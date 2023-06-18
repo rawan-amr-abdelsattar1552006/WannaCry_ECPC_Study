13813a35882eb943d948e4d00477a5550e1ea7b2
Rawan Amr Abdelsattar Abdelaal <64783197+rawan-amr-abdelsattar1552006@users.noreply.github.com>
2023-06-18 13:55:41 +0300

    Update ECPC STUDY.py , finished sheet #1 !

--- ECPC STUDY.py
+++ ECPC STUDY.py
@@ -52,13 +52,15 @@ print(int(nums[0][-1]) + int(nums[1][-1]))
 """
 
 """
-#### Sheet #1 Problem G
+#Sheet #1 Problem G
 
-num = int(input())
+n = int(input())
 
-print(sum(i for i in range(1,num + 1)))
+Sum = (n * (n+1)) / 2
+
+print(int(Sum))
+
 """
-
 
 """
 # Sheet #1 Problem H
@@ -146,6 +148,238 @@ else:
         print("IS SMALL")
 
 """
+"""
+# Sheet #1 Problem N
+
+char = input()
+
+if char.isupper():
+    print(char.lower())
+else:
+    print(char.upper())
+"""
+
+"""
+# Sheet #1 Problem O
+
+op = input()
+
+if "+" in op:
+    a,b = [int(x) for x in op.split("+")]
+    print(a + b)
+elif "-" in op:
+    a,b = [int(x) for x in op.split("-")]
+    print(a - b)
+elif "*" in op:
+    a,b = [int(x) for x in op.split("*")]
+    print(a * b)
+else:
+    a,b = [int(x) for x in op.split("/")]
+    print(a//b)
+"""
+
+"""
+# Sheet #1 Problem P
+
+num = input()
+
+if int(num[0])% 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")
+"""
+
+"""
+# Sheet #1 Problem Q
+
+x,y = [float(i) for i in input().split(" ")]
+
+if x == 0 and y == 0:
+    print("Origem")
+elif x == 0:
+    print("Eixo Y")
+elif y == 0:
+    print("Eixo X")
+elif x > 0 and y > 0:
+    print("Q1")
+elif x > 0 and y < 0:
+    print("Q4")
+elif x < 0 and y < 0:
+    print("Q3")
+elif x < 0 and y > 0:
+    print("Q2")
+"""
+
+"""
+# Sheet #1 Problem R
+
+days = int(input())
+
+years = days // 365
+months = (days - years * 365) // 30
+day = days - years * 365 - months * 30
+
+print(f"{years} years\n{months} months\n{day} days")
+
+"""
+
+"""
+# Sheet #1 Problem S
+
+num = float(input())
+
+if num >= 0 and num <= 25:
+    print("Interval [0,25]")
+elif num > 25 and num <= 50:
+    print("Interval (25,50]")
+elif num > 50 and num <= 75:
+    print("Interval (50,75]")
+elif num > 75 and num <= 100:
+    print("Interval (75,100]")
+else:
+    print("Out of Intervals")
+
+"""
+
+"""
+# Sheet #1 Problem T
+
+nums = [int(num) for num in input().split(" ")]
+
+for num in sorted(nums):
+    print(num)
+    
+print()
+
+for num in nums:
+    print(num)
+    
+"""
+
+"""
+# Sheet #1 Problem U
+
+num = input().split(".")
+
+if float(f"0.{num[1]}") == 0:
+    print(f"int {num[0]}")
+else:
+    print(f"float {num[0]} 0.{num[1]}")
+"""
+
+"""
+# Sheet #1 Problem V
+
+comp = input()
+
+if "<" in comp:
+    a,b = [int(i) for i in comp.split(" < ")]
+    if a < b:
+        print("Right")
+    else:
+        print("Wrong")
+        
+elif ">" in comp:
+    a,b = [int(i) for i in comp.split(" > ")]
+    if a > b:
+        print("Right")
+    else:
+        print("Wrong")
+
+elif "=" in comp:
+    a,b = [int(i) for i in comp.split(" = ")]
+    if a == b:
+        print("Right")
+    else:
+        print("Wrong")
+
+"""
+
+"""
+# Sheet #1 Problem W
+
+eq = input().replace(" ","").split("=")
+c = int(eq[1])
+
+if "+" in eq[0]:
+    a,b= [int(x) for x in eq[0].split("+")]
+    if a + b == c:
+        print("Yes")
+
+    else:
+        print(a + b)
+
+elif "-" in eq[0]:
+    a,b = [int(x) for x in eq[0].split("-")]
+    if a - b == c:
+        print("Yes")
+
+    else:
+        print(a - b)
+
+elif "*" in eq[0]:
+    a,b = [int(x) for x in eq[0].split("*")]
+    if a * b == c:
+        print("Yes")
+
+    else:
+        print(a * b)
+
+"""
+
+"""
+# Sheet #1 Problem X
+
+a1,b1,a2,b2 = [int(x) for x in input().split(" ")]
+boundaries = []
+
+if a1 >= a2 and a1 <= b2:
+    boundaries.append(a1)
+    if b1 >= a2 and b1 <= b2:
+        boundaries.append(b1)
+    elif b2 >= a1 and b2 <= b1:
+        boundaries.append(b2)
+    print(f"{boundaries[0]} {boundaries[1]}")
+    
+elif a2 >= a1 and a2 <= b1:
+    boundaries.append(a2)
+    if b1 >= a2 and b1 <= b2:
+        boundaries.append(b1)
+    elif b2 >= a1 and b2 <= b1:
+        boundaries.append(b2)
+    print(f"{boundaries[0]} {boundaries[1]}")
+
+else:
+    print(-1)
+"""
+
+"""
+#Sheet #1 Problem Y
+
+a,b,c,d = [int(x) for x in input().split(" ")]
+
+product = str(a * b * c * d)
+
+print(product[-2:])
+"""
+
+"""
+#Sheet #1 Problem Z
+
+a,b,c,d = [int(x) for x in input().split(" ")]
+
+if a == c and b > d and a!=1:
+    print("YES")
+elif a > c and b >= d:
+    print("YES")
+elif a==c and b==d:
+    print("NO")
+elif (a)**(b) > (c)**(d):
+    print("YES")
+else:
+    print("NO")
+"""
+
 
 """
 # ICPC Assiut : Contest#1 Problem A
@@ -266,43 +500,6 @@ elif a % b == 0 or b % a == 0:
 else:
     print("NO")
 """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
