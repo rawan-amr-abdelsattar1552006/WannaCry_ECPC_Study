0b24605a4588a7d09a8fa80c8b5a22f34366b372
Rawan Amr Abdelsattar Abdelaal <64783197+rawan-amr-abdelsattar1552006@users.noreply.github.com>
2023-06-22 17:08:42 +0300

    Update ECPC STUDY.py
    Some more problems solved !

--- ECPC STUDY.py
+++ ECPC STUDY.py
@@ -1,3 +1,4 @@
+# WANNACRY ?! No problem, it's solved !
 
 # Sheet #1 Problem A
 
@@ -464,9 +465,50 @@ print(a ^ b)
 """
 
 
-
+"""
 
 # ICPC Assiut : Contest#1 Problem G
+
+eyes, mouths, bodies = [int(x) for x in input().split(" ")]
+looping = True
+largest_possible_number = 0
+
+while looping:
+    first_method = min([eyes // 2 , bodies])
+    second_method = min([eyes // 2 , mouths, bodies])
+    third_method = min([eyes, mouths, bodies])
+
+    methods = [first_method, second_method, third_method]
+    
+    current_max = max(methods)
+
+    print(largest_possible_number, current_max)
+    largest_possible_number += current_max
+    
+    
+    index_of_method = methods.index(current_max)
+
+    if current_max == 0:
+        looping = False
+    
+    elif index_of_method == 0:
+        eyes -= (current_max * 2)
+        bodies -= current_max
+        
+    elif index_of_method == 1:
+        eyes -= (2 * current_max)
+        mouths -= current_max
+        bodies -= current_max
+    else:
+        eyes -= current_max
+        mouths -= current_max
+        bodies -= current_max
+
+    print(eyes, mouths, bodies)
+    
+
+print(largest_possible_number)
+"""
 
 """
 # ICPC Assiut : Contest#1 Problem H
@@ -501,5 +543,225 @@ else:
     print("NO")
 """
 
+"""
+# Sheet #2 Problem A
 
+n = int(input())
+
+for i in range(1,n+1):
+    print(i)
+"""
+
+"""
+# Sheet #2 Problem B
+n = int(input())
+
+even = [i for i in range(2, n + 1) if i % 2 == 0]
+
+if even:
+    for i in even:
+        print(i)
+else:
+    print(-1)
+"""
+"""
+# Sheet #2 Problem C
+
+num_of_values = int(input())
+values = [int(i) for i in input().split()]
+
+even = [i for i in values if i % 2 == 0]
+odd = [i for i in values if i % 2 != 0]
+positive = [i for i in values if i > 0]
+negative = [i for i in values if i < 0]
+
+print(f"Even: {len(even)}")
+print(f"Odd: {len(odd)}")
+print(f"Positive: {len(positive)}")
+print(f"Negative: {len(negative)}")
+
+"""
+"""
+# Sheet #2 Problem D
+
+trying = True
+while trying:
+    passwd = input()
+    if passwd == "1999":
+        print("Correct")
+        break
+    else:
+        print("Wrong")
+
+"""
+"""
+# Sheet #2 Problem E
+
+num_of_values = int(input())
+values = [int(i) for i in input().split(" ")]
+
+max_num = values[0]
+
+for num in values:
+    if num > max_num:
+        max_num = num
+print(max_num)
+"""
+
+"""
+# Sheet #2 Problem F
+n = int(input())
+
+for i in range(1, 13):
+    print(f"{n} * {i} = {n * i}")
+
+"""
+
+"""
+# Sheet #2 Problem G
+num_of_testcases = int(input())
+
+for i in range(num_of_testcases):
+    n = int(input())
+    factorial = 1
+
+    if n == 0:
+        print(1)
+    else:
+        for j in range(2,n + 1):
+            factorial *= j
+        print(factorial)
+
+"""
+"""
+# Sheet #2 Problem H
+import math
+
+num = int(input())
+sqrt_of_num = int(math.sqrt(num))
+isPrime = "YES"
+ 
+if num % 2 == 0 and num != 2:
+    isPrime = "NO"
+else:
+    for i in range(3,sqrt_of_num + 1,2):
+        if num % i == 0:
+            isPrime = "NO"
+print(isPrime)
+
+"""
+
+"""
+# Sheet #2 Problem I
+
+num = input()
+reverse = num[::-1]
+
+print(int(reverse))
+
+if num == reverse:
+    print("YES")
+else:
+    print("NO")
+
+"""
+
+"""
+# Sheet 2 Problem J
+import math
+num = int(input())
+primes = ["2"]
+
+def isPrime(x):
+    sqrt_of_num = int(math.sqrt(x))
+    isPrime = True
+     
+    if x % 2 == 0 and x != 2:
+        isPrime = False
+    else:
+        for i in range(3,sqrt_of_num + 1,2):
+            if x % i == 0:
+                isPrime = False
+    return isPrime
+
+
+for i in range(3,num + 1, 2):
+    if isPrime(i):
+        primes.append(str(i))
+
+print(" ".join(primes))
+"""
+
+"""
+# Sheet 2 Problem K
+
+n = int(input())
+
+for i in range(1,n + 1):
+    if n % i == 0:
+        print(i)
+"""
+
+# Sheet 2 Problem L
+
+"""
+# Sheet 2 Problem M
+
+a,b = [int(i) for i in input().split(" ")]
+lucky = []
+
+def isLucky(x):
+    for i in str(x):
+        if i not in ["4","7"]:
+            return False
+       
+    return True
+
+for i in range(a,b + 1):
+    if isLucky(i):
+        lucky.append(str(i))
+
+print(" ".join(lucky) if lucky else "-1")
+
+"""
+"""
+# Sheet 2 Problem N
+
+symbol = input()
+n = int(input())
+values = [int(x) for x in input().split(" ")]
+
+for i in range(n):
+    print(symbol * values[i])
+
+"""
+"""
+# Sheet 2 Problem O
+
+symbol = input()
+n = int(input())
+values = [int(x) for x in input().split(" ")]
+
+for i in range(n):
+    print(symbol * values[i])
+"""
+
+"""
+# Sheet 2 Problem P
+
+n = int(input())
+
+for i in range(n, 0, -1):
+    print("*" * i)
+"""
+
+"""
+# Sheet 2 Problem Q
+n = int(input())
+
+for i in range(n):
+     num = input()
+     reverse = list(num[::-1])
+     print(" ".join(reverse))
+"""
 
